2fb9591a699c3664724b7bff85c1c3b673704be8
Akash <you@example.com>
2022-12-20 13:48:43 +0530

    Portfolio v1.0.2

--- dist/js/project_data.js
+++ dist/js/project_data.js
@@ -4,84 +4,84 @@ let projects=
     {
         name:"Swagshop",
         tools:"HTML, CSS & JS",
-        imgSource:"/dist/img/websites/swagshop.png",
+        imgSource:"./dist/img/websites/swagshop.png",
         url:"https://aklu-10.github.io/SwagShop-WebDesign/",
         type:"website"
     },
     {
         name:"Webder",
         tools:"HTML, CSS & JS",
-        imgSource:"/dist/img/websites/webder.png",
+        imgSource:"./dist/img/websites/webder.png",
         url:"https://aklu-10.github.io/Webder-WebDesign/",
         type:"website"
     },
     {
         name:"Rectarc",
         tools:"HTML, CSS & JS",
-        imgSource:"/dist/img/websites/rectarc.png",
+        imgSource:"./dist/img/websites/rectarc.png",
         url:"https://aklu-10.github.io/Rectarc-WebDesign/",
         type:"website"
     },
     {
         name:"Ebuy",
         tools:"HTML, CSS & Bootstrap",
-        imgSource:"/dist/img/websites/ebuy.png",
+        imgSource:"./dist/img/websites/ebuy.png",
         url:"https://aklu-10.github.io/Ebuy-WebDesign/",
         type:"website"
     },
     {
         name:"Sugary Design",
         tools:"Adobe Illustrator",
-        imgSource:"/dist/img/illus/background2.png",
+        imgSource:"./dist/img/illus/background2.png",
         url:"/dist/img/illus/background2.png",
         type:"illus"
     },
     {
         name:"Geometric Design",
         tools:"Adobe Illustrator",
-        imgSource:"/dist/img/illus/bird.png",
+        imgSource:"./dist/img/illus/bird.png",
         url:"/dist/img/illus/bird.png",
         type:"illus"
     },
     {
         name:"Desert",
         tools:"Adobe Illustrator & Silhouette",
-        imgSource:"/dist/img/illus/desert.jpg",
+        imgSource:"./dist/img/illus/desert.jpg",
         url:"/dist/img/illus/desert.jpg",
         type:"illus"
     },
     {
         name:"Wallpaper",
         tools:"Adobe Illustrator & Silhouette",
-        imgSource:"/dist/img/illus/screensaver.png",
+        imgSource:"./dist/img/illus/screensaver.png",
         url:"/dist/img/illus/screensaver.png",
         type:"illus"
     },
     {
         name:"Scenery",
         tools:"Adobe Illustrator & Silhouette",
-        imgSource:"/dist/img/illus/wolf_at_night.jpg",
+        imgSource:"./dist/img/illus/wolf_at_night.jpg",
         url:"/dist/img/illus/wolf_at_night.jpg",
         type:"illus"
     },
     {
         name:"Scenery",
         tools:"Adobe Illustrator & Silhouette",
-        imgSource:"/dist/img/illus/sunset.jpg",
+        imgSource:"./dist/img/illus/sunset.jpg",
         url:"/dist/img/illus/sunset.jpg",
         type:"illus"
     },
     {
         name:"React NoteKeeper",
         tools:"Reactjs",
-        imgSource:"/dist/img/other/react_notekeeper.png",
+        imgSource:"./dist/img/other/react_notekeeper.png",
         url:"https://quicknotekeeper.netlify.app/",
         type:"other"
     },
     {
         name:"React DigitalClock",
         tools:"Reactjs",
-        imgSource:"/dist/img/other/react_digiclock.png",
+        imgSource:"./dist/img/other/react_digiclock.png",
         url:"https://vocal-paprenjak-3e789b.netlify.app/",
         type:"other"
 
@@ -89,7 +89,7 @@ let projects=
     {
         name:"React CryptoTable",
         tools:"Reactjs",
-        imgSource:"/dist/img/other/react_cryptotable.png",
+        imgSource:"./dist/img/other/react_cryptotable.png",
         url:"https://incredible-shortbread-58b6b8.netlify.app/",
         type:"other"
     }
